e6ce2039ae9d0af9a382001ac09ab3b9b37b1968
Bruno <bruno@doty2.df.ibilce.unesp.br>
2017-04-20 15:18:43 -0300

    Execução do pulo_do_gato.py e criação dos symlinks para executar na pasta do job

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from werkzeug.utils import secure_filename
 import os
 import hashlib
 from datetime import datetime
+import subprocess
+import glob
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -27,13 +29,32 @@ def submit():
         if 'input_file' not in request.files:
             return redirect(request.url)
 
-        print(request.files.get('input_file').filename)
-        file = request.files.get('input_file')
-        # filename = hashlib.sha512(file.read()).hexdigest() + '.pdb'
-        filename = datetime.today().strftime('%Y-%m-%d_%H:%M:%S:%f_') + secure_filename(file.filename)
+        job_dir = 'jobs/{}'.format(datetime.today().strftime('%Y-%m-%d_%H:%M:%S:%f'))
+        os.makedirs(job_dir)
+        [ os.symlink(os.path.join('../../pulo_do_gato_bin', f), os.path.join(job_dir,f)) for f in os.listdir('pulo_do_gato_bin') ]
 
-        if not os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return request.form.get('temperature')
+
+        file = request.files.get('input_file')
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(job_dir, filename))
+
+        cwd = os.getcwd()
+        os.chdir(job_dir)
+        print(os.getcwd())
+        subprocess.call('python2 ./pulo_do_gato.py -T 20 -ph 3.4 -s MC -f 5vab.pdb > output.txt', shell=True)
+
+        output_file = open('output.txt', 'r')
+        image_filename = os
+        return_data = jsonify(
+            job_dir=job_dir,
+            stdout=output_file.read(),
+            image=glob.glob('*.jpg')[0],
+            output=glob.glob('Output*.dat')[0]
+        )
+        output_file.close()
+
+        os.chdir(cwd)
+
+        return return_data
     else:
         return ''
