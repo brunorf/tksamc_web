e073bb7ce3755cc5890b9d01217b1574970bae9c
Bruno <bruno@doty2.df.ibilce.unesp.br>
2017-04-20 11:53:07 -0300

    Fim dos testes de upload

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
-from flask import Flask
-from flask import render_template
-from flask import Response
-from flask import jsonify
-from flask import request
-
+from flask import Flask, render_template, Response, jsonify, request
+from werkzeug.utils import secure_filename
+import os
+import hashlib
+from datetime import datetime
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['pdb'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -13,9 +22,18 @@ def index():
 
 @app.route('/submit', methods=['GET','POST', 'PUT'])
 def submit():
-    # return request.args.get('pH', '')
     if request.method == 'POST':
-        print(request.form.get('pH'))
-        return ''
+        # check if the post request has the file part
+        if 'input_file' not in request.files:
+            return redirect(request.url)
+
+        print(request.files.get('input_file').filename)
+        file = request.files.get('input_file')
+        # filename = hashlib.sha512(file.read()).hexdigest() + '.pdb'
+        filename = datetime.today().strftime('%Y-%m-%d_%H:%M:%S:%f_') + secure_filename(file.filename)
+
+        if not os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return request.form.get('temperature')
     else:
         return ''
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
   $('form#submit_job_form').submit(function(event) {
-    // var formData = new FormData($(this)[0]);
-    var data = {
-      pH: 3,
-    }
-    $.post($(this).attr('action'), data ,function(data) {
-
+    var form_data = new FormData();
+    form_data.append('pH', $('input[name=pH]').val());
+    form_data.append('temperature', $('input[name=temperature]').val());
+    form_data.append('input_file', $('input[name=input_file]')[0].files[0]);
+    // $.post($(this).attr('action'), form_data ,function(data) {
+    //   alert(data);
+    // });
+    $.ajax({
+      url: $(this).attr('action'),
+      data: form_data,
+      type: 'POST',
+      contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
+      processData: false, // NEEDED, DON'T OMIT THIS
+      success: function(data) {
+        alert(data);
+      },
     });
     return false;
   });
