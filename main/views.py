26c9f04cad03a44e658892db9f29237dfc59c9c4
Bruno <bruno@doty2.df.ibilce.unesp.br>
2017-06-13 09:16:22 -0300

    Criação do app main e modificação do urls de acordo com isso

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    # return render(request, 'blog/post_list.html', {})
+    return HttpResponse('teste')
