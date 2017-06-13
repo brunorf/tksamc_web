13d542455d4a098ad07d8e3d9414d89347f3f2de
Bruno <bruno@doty2.df.ibilce.unesp.br>
2017-06-13 15:17:23 -0300

    Melhorias na apresentação e validação do formulário

--- main/forms.py
+++ main/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class JobForm(forms.Form):
     pdb_file = forms.FileField()
-    name = forms.CharField(label='Job name', max_length=30, required=False)
-    email = forms.EmailField(label='E-mail', required=False)
-    ph_range = forms.BooleanField(label='Use pH Range', required=False)
-    ph = forms.DecimalField(label='pH', max_digits=2, decimal_places=1)    
-    temperature = forms.DecimalField(label='Temperature', max_digits=10, decimal_places=2)
+    name = forms.CharField(label='Job name', max_length=30, required=False, help_text='Optional')
+    email = forms.EmailField(label='E-mail', required=False, help_text='Optional')
+    ph_range = forms.BooleanField(label='Use pH Range', required=False, help_text='Check if you want to run on a pH range from 2 to 12, with 0.5 step')
+    ph = forms.DecimalField(label='pH', max_digits=2, decimal_places=1, min_value=0, max_value=14)
+    temperature = forms.DecimalField(label='Temperature', max_digits=10, decimal_places=2, min_value=0)
