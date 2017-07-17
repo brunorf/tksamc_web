698bab11d523b9319d30bf8509d7dd7d63e1e8b8
Bruno Rogerio Fernandes <bruno@sjrp.unesp.br>
2017-07-17 15:55:00 -0300

    Finalização das views submit e check_job

--- main/views.py
+++ main/views.py
@@ -41,10 +41,15 @@ def submit(request):
         with helpers.change_workingdir(job_dir):
             subprocess.Popen('gmx editconf -f {0} -c -resnr 1 -label A -o processed_{0}'.format(pdb_file.name), shell=True)
 
-            if job.ph_range:
-                subprocess.Popen('./run_pdg-ph.sh processed_{} MC > output.txt; touch finished'.format(pdb_file.name.split('.')[0]), shell=True)
+            if job.name != '':
+                archive_name = job.name
             else:
-                subprocess.Popen('python2 ./pulo_do_gato.py -T {} -ph {} -s MC -f processed_{} > output.txt; touch finished'.format(temperature, ph, pdb_file.name), shell=True)
+                archive_name = str(job.id)
+
+            if job.ph_range:
+                subprocess.Popen('./run_pdg-ph.sh processed_{} MC {} > output.txt; touch finished'.format(pdb_file.name.split('.')[0], archive_name), shell=True)
+            else:
+                subprocess.Popen('./run_pdg.sh {} {} {} {}'.format(temperature, ph, pdb_file.name, archive_name), shell=True)
 
             if email != '':
                 send_email(email, job_id)
@@ -53,4 +58,47 @@ def submit(request):
 
 
 def check_job(request, job_id):
-    return HttpResponse(job_id)
+    import os
+    import glob
+    import subprocess
+
+    job_dir = os.path.join('jobs', str(job_id) )
+    finished = False
+
+    job = Job.objects.filter(id=job_id).first()
+    if job == None:
+        return HttpResponse('404')
+
+    job_data = dict()
+    if os.path.isfile( os.path.join(job_dir, 'finished') ):
+        finished = True
+        stdout = ''
+        # with open(os.path.join(job_dir,'output.txt'), encoding='utf-8') as stdout_file:
+        #     stdout='<br/>'.join(stdout_file.read().split('\n')),
+
+        # print(job.pH_range == False)
+        if job.ph_range:
+            image1 = os.path.basename(glob.glob(os.path.join(job_dir,'Fig_Gqq*.jpg') )[0])
+            image2 = os.path.basename(glob.glob(os.path.join(job_dir,'*pH_7.0*.jpg') )[0])
+            stdout = subprocess.check_output("grep -e 'T\s=*' {0} | tail -1; grep 'Total dG Energy' {0} | tail -1".format(os.path.join(job_dir,'output.txt')), shell=True, universal_newlines=True)
+        else:
+            image1 = os.path.basename(glob.glob(os.path.join(job_dir,'*.jpg') )[0])
+            image2 = None
+            stdout = (subprocess.check_output("grep -e 'pH\s=*' {0}; grep -e 'T\s=*' {0}; grep 'Total dG Energy' {0}".format(os.path.join(job_dir,'output.txt')), shell=True, universal_newlines=True).split('\n'))
+
+        stdout = '<br/>'.join(stdout)
+        if job.name != '':
+            archive_name = job.name
+        else:
+            archive_name = str(job.id)
+        job_data = dict(
+            job_id=str(job_id),
+            output_file=os.path.basename(glob.glob(os.path.join(job_dir, 'Output*.dat'))[0]),
+            image1=image1,
+            image2=image2,
+            stdout=stdout,
+            ph_range=job.ph_range,
+            archive_name=archive_name
+        )
+
+    return render(request, 'main/check_job.html', {'finished':finished, **job_data})
