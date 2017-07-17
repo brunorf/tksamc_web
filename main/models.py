8ffda08b2690b7ee546f5208cafd9ece6551da72
Bruno Rogerio Fernandes <bruno@sjrp.unesp.br>
2017-07-17 19:28:10 -0300

    Alteração do campo pH para conter até 3 digitos

--- main/models.py
+++ main/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Job(models.Model):
     name = models.CharField(max_length=30)
-    ph = models.DecimalField(max_digits=2, decimal_places=1)
+    ph = models.DecimalField(max_digits=3, decimal_places=1)
     ph_range = models.BooleanField()
     temperature = models.DecimalField(max_digits=10, decimal_places=2)
     submit_date = models.DateTimeField(auto_now_add=True)
     email = models.EmailField()
 
     def __str__(self):
-        return '<Job (%r) - %r>' % (self.id, self.job_name)
+        return '<Job (%r) - %r>' % (self.id, self.name)
