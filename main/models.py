6cde30dce1356630a2d7df5ea9f580cc40a63d22
Bruno <bruno@rambo.df.ibilce.unesp.br>
2017-09-12 08:52:10 -0300

    Alteração da id do job para um UUID

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30)
     ph = models.DecimalField(max_digits=3, decimal_places=1)
     ph_range = models.BooleanField()
