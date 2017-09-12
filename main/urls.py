8d8534bc4e8baacc3a50abc92ffba97e4d9f857f
Bruno <bruno@rambo.df.ibilce.unesp.br>
2017-09-12 09:36:27 -0300

    Mudança da url para check_job para aceitar UUID

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^results$', views.results, name='results'),
     url(r'^theory$', views.theory, name='theory'),
     url(r'^submit$', views.submit, name='submit'),
-    url(r'^check_job/([0-9]{,4})$', views.check_job, name='check_job'),
+    url(r'^check_job/([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z)$', views.check_job, name='check_job'),
     url(r'^googlecc98d7b5a37f2c7d.html$', views.google_search_console, name='google_search_console'),
     url(r'^sitemap.xml$', views.sitemap, name='sitemap'),
 ]
