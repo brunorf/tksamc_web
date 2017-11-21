97a6271752e41bbe5e46e0b1991902e9074d100e
Bruno Rogerio Fernandes <brogeriofernandes@gmail.com>
2017-11-21 09:15:16 -0200

    Correção do bug ao tentar submeter um job com pH range

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#id_ph_range').click(function() {
-    $('#id_ph').prop('disabled', $('#id_ph_range').is(':checked'))
+    $('#id_ph').prop('readonly', $('#id_ph_range').is(':checked'))
   })
 });
