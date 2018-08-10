6844f18348410c3b044f37d0d0577c1b70360412
Bruno Rogerio Fernandes <brogeriofernandes@gmail.com>
2018-08-10 09:31:35 -0300

    Inclusão de campo chain no model

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Job(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30)
+    chain = models.CharField(max_length=1, null=True)
     ph = models.DecimalField(max_digits=3, decimal_places=1)
     ph_range = models.BooleanField()
     temperature = models.DecimalField(max_digits=10, decimal_places=2)
