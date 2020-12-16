999bac3eae7a90901f24ddc30dd51a1d9d2c4c9c
Bruno Rogério Fernandes <brogeriofernandes@gmail.com>
2020-12-16 13:48:24 -0300

    Inclusão de campo tksamc_version

--- main/forms.py
+++ main/forms.py
@@ -12,3 +12,4 @@ class JobForm(forms.Form):
     ph_range = forms.BooleanField(label='Use pH Range', required=False, help_text='Check if you want to run on a pH range from 2 to 12, with 0.5 step')
     ph = forms.DecimalField(label='pH', max_digits=3, decimal_places=1, min_value=0, max_value=14, initial='7.0')
     temperature = forms.DecimalField(label='Temperature', max_digits=4, decimal_places=1, min_value=280, max_value=380, initial='300.0')
+    tksamc_version = forms.ChoiceField(required=True, choices=[(1, "Classic TKSA-MC"), (2, "NTKSA-MC"), (0, "Both")])
