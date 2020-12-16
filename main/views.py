cd13902334132d57bbf37b5487b3cc1d688f5050
Bruno Rogério Fernandes <brogeriofernandes@gmail.com>
2020-12-16 17:58:34 -0300

    tksamc_bin e ntksamc_bin

--- main/views.py
+++ main/views.py
@@ -70,12 +70,29 @@ def submit(request):
     from . import helpers
     from .forms import JobForm
     import pypdb
+    import shutil
 
     pdb_search = request.POST['pdb_search']
 
     pdb_file = None
     pdb_filename = None
     available_chains = []
+
+    
+    def run_script(processed_pdb_path, job_dir, orig_bin_dir, archive_name):
+        [os.symlink(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', orig_bin_dir, f),
+                    os.path.join(tksamc_job_dir, f)) for f in os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', orig_bin_dir))]
+
+        with helpers.change_workingdir(job_dir):
+            shutil.copyfile(processed_pdb_path, job_dir)
+
+            if job.ph_range:
+                subprocess.Popen(['./run_pdg-ph.sh', 'processed_{}'.format(
+                    pdb_filename.split('.')[0]), 'MC', archive_name], shell=False)
+            else:
+                subprocess.Popen(['./run_pdg.sh', str(temperature), str(ph),
+                                  'processed_{}'.format(pdb_filename), archive_name], shell=False)
+
     if (pdb_search):
         pdb = pypdb.get_pdb_file(pdb_search)
         pdb_filename = pdb_search + '.pdb'
@@ -99,12 +116,22 @@ def submit(request):
         ph_range = form.cleaned_data['ph_range']
         email = form.cleaned_data['email']
         chain = form.cleaned_data['chain']
+        tksamc_version = form.cleaned_data['tksamc_version']
 
         job = Job(name=name, ph=ph, ph_range=ph_range,
-                  temperature=temperature, email=email, chain=chain)
+                  temperature=temperature, email=email, chain=chain, tksamc_version=tksamc_version)
         job.save()
         job_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../static/jobs/', str(job.id))
         os.makedirs(job_dir)
+        
+        tksamc_job_dir = None
+        ntksamc_job_dir = None
+        if tksamc_version == 1 or tksamc_version == 0:
+            tksamc_job_dir = os.path.join(job_dir, 'TKSAMC')
+            os.makedirs(tksamc_job_dir)
+        elif tksamc_version == 2 or tksamc_version == 0:
+            ntksamc_job_dir = os.path.join(job_dir, 'NTKSAMC')
+            os.makedirs(ntksamc_job_dir)
 
         if (not chain):
             chain = ['^\s']
@@ -112,32 +139,29 @@ def submit(request):
         with open(os.path.join(job_dir, pdb_filename), 'w') as destination:
             new_pdb = '\n'.join(re.findall(r'^ATOM\s+(?:[^\s]+\s+){3}[%s]\s+.*' % ('|'.join(chain)), pdb, re.MULTILINE))
             destination.write(new_pdb)
-            
-        [os.symlink(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../pulo_do_gato_bin', f),
-                    os.path.join(job_dir, f)) for f in os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../pulo_do_gato_bin'))]
+        
+        subprocess.Popen(['/bin/sed', '-i', 's/AALA/ ALA/g;s/ACYS/ CYS/g;s/AASP/ ASP/g;s/AGLU/ GLU/g;s/APHE/ PHE/g;s/AGLY/ GLY/g;s/AHIS/ HIS/g;s/AILE/ ILE/g;s/ALYS/ LYS/g;s/ALEU/ LEU/g;s/AMET/ MET/g;s/AASN/ ASN/g;s/APRO/ PRO/g;s/AGLN/ GLN/g;s/AARG/ ARG/g;s/ASER/ SER/g;s/ATHR/ THR/g;s/AVAL/ VAL/g;s/ATRP/ TRP/g;s/ATYR/ TYR/g', pdb_filename], shell=False)
+        subprocess.Popen(['/bin/sed', '-i', '/BALA/d;/BCYS/d;/BASP/d;/BGLU/d;/BPHE/d;/BGLY/d;/BHIS/d;/BILE/d;/BLYS/d;/BLEU/d;/BMET/d;/BASN/d;/BPRO/d;/BGLN/d;/BARG/d;/BSER/d;/BTHR/d;/BVAL/d;/BTRP/d;/BTYR/d', pdb_filename], shell=False)
+        subprocess.Popen(['/usr/bin/gmx', 'editconf', '-f', pdb_filename, '-c', '-resnr',
+                            '1', '-label', 'A', '-o', 'processed_{0}'.format(pdb_filename)], shell=False)
+        
+        if job.name != '':
+            base_archive_name = job.name
+        else:
+            base_archive_name = str(job.id)
+        
+        if tksamc_job_dir:
+            run_script(os.path.join(job_dir, 'processed_{0}'.format(pdb_filename)),
+            tksamc_job_dir, 'tksamc_bin', 'tksamc_' + base_archive_name)
+        if ntksamc_job_dir:
+            run_script(os.path.join(job_dir, 'processed_{0}'.format(pdb_filename)),
+            ntksamc_job_dir, 'ntksamc_bin', 'ntksamc_' + base_archive_name)
+        
 
-        with helpers.change_workingdir(job_dir):
-            subprocess.Popen(['/bin/sed', '-i', 's/AALA/ ALA/g;s/ACYS/ CYS/g;s/AASP/ ASP/g;s/AGLU/ GLU/g;s/APHE/ PHE/g;s/AGLY/ GLY/g;s/AHIS/ HIS/g;s/AILE/ ILE/g;s/ALYS/ LYS/g;s/ALEU/ LEU/g;s/AMET/ MET/g;s/AASN/ ASN/g;s/APRO/ PRO/g;s/AGLN/ GLN/g;s/AARG/ ARG/g;s/ASER/ SER/g;s/ATHR/ THR/g;s/AVAL/ VAL/g;s/ATRP/ TRP/g;s/ATYR/ TYR/g', pdb_filename], shell=False)
-            subprocess.Popen(['/bin/sed', '-i', '/BALA/d;/BCYS/d;/BASP/d;/BGLU/d;/BPHE/d;/BGLY/d;/BHIS/d;/BILE/d;/BLYS/d;/BLEU/d;/BMET/d;/BASN/d;/BPRO/d;/BGLN/d;/BARG/d;/BSER/d;/BTHR/d;/BVAL/d;/BTRP/d;/BTYR/d', pdb_filename], shell=False)
-            subprocess.Popen(['/usr/bin/gmx', 'editconf', '-f', pdb_filename, '-c', '-resnr',
-                              '1', '-label', 'A', '-o', 'processed_{0}'.format(pdb_filename)], shell=False)
-
-            if job.name != '':
-                archive_name = job.name
-            else:
-                archive_name = str(job.id)
-
-            if job.ph_range:
-                subprocess.Popen(['./run_pdg-ph.sh', 'processed_{}'.format(
-                    pdb_filename.split('.')[0]), 'MC', archive_name], shell=False)
-            else:
-                subprocess.Popen(['./run_pdg.sh', str(temperature), str(ph),
-                                  'processed_{}'.format(pdb_filename), archive_name], shell=False)
-
-            if email != '':
-                job_url = request.build_absolute_uri(
-                    reverse('check_job', args=[job.id]))
-                helpers.send_email(email, job.name, job_url)
+        if email != '':
+            job_url = request.build_absolute_uri(
+                reverse('check_job', args=[job.id]))
+            helpers.send_email(email, job.name, job_url)
 
         return HttpResponseRedirect(reverse('check_job', args=[job.id]))
     else:
